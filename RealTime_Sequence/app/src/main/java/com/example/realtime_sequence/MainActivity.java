cc2dc152c0a79cb753017573696e204ef93d5365
owl-d <limdoyou000921@gmail.com>
2022-03-08 21:15:42 +0900

    commit

--- RealTime_Sequence/app/src/main/java/com/example/realtime_sequence/MainActivity.java
+++ RealTime_Sequence/app/src/main/java/com/example/realtime_sequence/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.realtime_sequence;
 import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.gun0912.tedpermission.PermissionListener;
@@ -19,49 +26,93 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Camera camera;
-    private SurfaceView surfaceView;
-    private SurfaceHolder surfaceHolder;
+    private static CameraPreview surfaceView;
+    private SurfaceHolder holder;
+    private static Button camera_preview_button;
+    private static Camera mCamera;
+    private int RESULT_PERMISSIONS = 100;
+    public static MainActivity getInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 카메라 프리뷰를  전체화면으로 보여주기 위해 셋팅한다.
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        // 안드로이드 6.0 이상 버전에서는 CAMERA 권한 허가를 요청한다.
+        //권한 설정
+        requestPermissionCamera();
+
+//        TedPermission.with(this)
+//                .setPermissionListener(permission)
+//                .setRationaleMessage("녹화를 위하여 권한을 허용해주세요.")
+//                .setDeniedMessage("권한이 거부되었습니다. 설정 > 권한에서 허용해주세요.")
+//                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO)
+//                .check();
+    }
+    public static Camera getCamera(){
+        return mCamera;
+    }
+    private void setInit(){
+        getInstance = this;
+
+        // 카메라 객체를 R.layout.activity_main의 레이아웃에 선언한 SurfaceView에서 먼저 정의해야 함으로 setContentView 보다 먼저 정의한다.
+        mCamera = Camera.open();
+
         setContentView(R.layout.activity_main);
 
-        //권한 설정
-        TedPermission.with(this)
-                .setPermissionListener(permission)
-                .setRationaleMessage("녹화를 위하여 권한을 허용해주세요.")
-                .setDeniedMessage("권한이 거부되었습니다. 설정 > 권한에서 허용해주세요.")
-                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO)
-                .check();
+        // SurfaceView를 상속받은 레이아웃을 정의한다.
+        surfaceView = (CameraPreview) findViewById(R.id.preview);
 
-        qopen = safeCameraOpen(id); //카메라 열기
 
+        // SurfaceView 정의 - holder와 Callback을 정의한다.
+        holder = surfaceView.getHolder();
+        holder.addCallback(surfaceView);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
-    private boolean safeCameraOpen(int id) {
-        boolean qOpened = false;
+    public boolean requestPermissionCamera(){
+        int sdkVersion = Build.VERSION.SDK_INT;
+        if(sdkVersion >= Build.VERSION_CODES.M) {
 
-        try {
-            releaseCameraAndPreview();
-            camera = Camera.open(id); //카메라 객체 열기
-            Log.d(TAG, "Camera Open");
-            qOpened = (camera != null);
-        } catch (Exception e) {
-            Log.e(getString(R.string.app_name), "failed to open Camera");
-            e.printStackTrace();
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        RESULT_PERMISSIONS);
+
+            }else {
+                setInit();
+            }
+        }else{  // version 6 이하일때
+            setInit();
+            return true;
         }
 
-        return qOpened;
+        return true;
     }
 
-    private void releaseCameraAndPreview() {
-        preview.setCamera(null);
-        if (camera != null) {
-            camera.release();
-            camera = null;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (RESULT_PERMISSIONS == requestCode) {
+
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한 허가시
+                setInit();
+            } else {
+                // 권한 거부시
+            }
+            return;
         }
+
     }
 
     PermissionListener permission = new PermissionListener() {
